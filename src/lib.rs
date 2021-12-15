5c5a1d358f94f1bdc59b947fa1e24d2ea90f172f
KirFed <f3lkilo@yandex.ru>
2021-12-15 11:47:13 +0300

    Basic ThreadPool realization.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,59 @@
+use std::sync::mpsc::Sender;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread::JoinHandle;
+use std::{mem, thread};
+
+type Task = Box<dyn FnOnce() + Send + 'static>;
+
 #[derive(Debug, Clone)]
-pub struct ThreadPool;
+pub struct ThreadPool {
+    handles: Arc<Vec<JoinHandle<()>>>,
+    sender: Option<Sender<Task>>,
+}
 
 impl ThreadPool {
-    pub fn new(_threads: u32) -> Option<Self> {
-        todo!()
+    pub fn new(threads_count: usize) -> Option<Self> {
+        if threads_count == 0 {
+            return None;
+        }
+
+        let (sender, rx) = mpsc::channel::<Task>();
+        let rx = Arc::new(Mutex::new(rx));
+
+        let mut handles = Vec::with_capacity(threads_count);
+        for _ in 0..threads_count {
+            let rx = rx.clone();
+            let jh = thread::spawn(move || {
+                while let Ok(task) = rx.lock().unwrap().recv() {
+                    task()
+                }
+            });
+            handles.push(jh);
+        }
+
+        Some(Self {
+            handles: Arc::new(handles),
+            sender: Some(sender),
+        })
     }
 
-    pub fn spawn<F>(&self, _f: F)
+    pub fn spawn<F>(&self, f: F)
     where
-        F: FnOnce() + 'static,
+        F: FnOnce() + Send + 'static,
     {
-        todo!()
+        let task = Box::new(f);
+        let _ = self.sender.as_ref().unwrap().send(task);
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        let arc = mem::replace(&mut self.handles, Arc::new(Default::default()));
+        if let Ok(handles) = Arc::try_unwrap(arc) {
+            mem::drop(self.sender.take());
+            for jh in handles {
+                jh.join().unwrap();
+            }
+        }
     }
 }
