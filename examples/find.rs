340d9ed4d63fe850717bc861b52700dca1bd2f0c
KirFed <f3lkilo@yandex.ru>
2021-12-15 14:28:37 +0300

    Add rusty-pool crate.

--- examples/find.rs
+++ examples/find.rs
@@ -24,7 +24,17 @@ fn main() {
     println!("------------------",);
     println!("Testing mt_pool_find with {}", to_find);
     let pool = ThreadPool::new(40).unwrap();
-    let found = measure(|| mt_pool_find(data, to_find, pool));
+    let data_clone = Arc::clone(&data);
+    let found = measure(|| mt_pool_find(data_clone, to_find, pool));
+    println!("found: {:?}", found);
+
+    thread::sleep(Duration::from_secs(2));
+
+    println!("------------------",);
+    println!("Testing mt_rusty_pool_find with {}", to_find);
+    let pool = rusty_pool::ThreadPool::new(40, 40, Duration::from_secs(60));
+    let data_clone = Arc::clone(&data);
+    let found = measure(|| mt_rusty_pool_find(data_clone, to_find, pool));
     println!("found: {:?}", found);
 }
 
@@ -36,7 +46,7 @@ fn measure<T>(f: impl FnOnce() -> T) -> T {
     result
 }
 
-const CHUNK_SIZE: usize = 500_000;
+const CHUNK_SIZE: usize = 50_000;
 
 fn st_find(data: &[i32], val: i32) -> Option<usize> {
     data.iter()
@@ -88,6 +98,39 @@ fn mt_pool_find(data: Arc<[i32]>, val: i32, pool: ThreadPool) -> Option<usize> {
         let tx = tx.clone();
         let data = data.clone();
         pool.spawn(move || {
+            let chunk_start = chunk * CHUNK_SIZE;
+            let chunk_end = (chunk + 1) * CHUNK_SIZE;
+            let data = &data[chunk_start..chunk_end];
+
+            let found = data
+                .iter()
+                .enumerate()
+                .find(|(_, v)| **v == val)
+                .map(|(i, _)| chunk_start + i);
+
+            let _ = tx.send(found);
+        });
+    }
+
+    for _ in chunks_range {
+        if let Some(found) = rx.recv().unwrap() {
+            return Some(found);
+        }
+    }
+
+    None
+}
+
+fn mt_rusty_pool_find(data: Arc<[i32]>, val: i32, pool: rusty_pool::ThreadPool) -> Option<usize> {
+    let chunks_count = data.len() / CHUNK_SIZE;
+    let chunks_range = 0..chunks_count;
+
+    let (tx, rx) = mpsc::channel();
+
+    for chunk in chunks_range.clone() {
+        let tx = tx.clone();
+        let data = data.clone();
+        pool.execute(move || {
             let chunk_start = chunk * CHUNK_SIZE;
             let chunk_end = (chunk + 1) * CHUNK_SIZE;
             let data = &data[chunk_start..chunk_end];
