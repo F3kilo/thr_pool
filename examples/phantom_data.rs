d344831eb931eaf0d0fd96044ab7d03a1d70435e
f3 <f3kilo@yandex.ru>
2023-05-29 19:51:10 +0300

    minor update

--- examples/phantom_data.rs
+++ examples/phantom_data.rs
@@ -1,13 +1,15 @@
+#![allow(dead_code, unused)]
+
 use std::marker::PhantomData;
 
 fn main() {
-    let _a = BytesRepr::<u32>{
-        _bytes: 42u32.to_be_bytes().to_vec(),
-        _p: PhantomData
+    let a = BytesRepr::<u32>{
+        bytes: 42u32.to_be_bytes().to_vec(),
+        p: PhantomData
     };
 }
 
 struct BytesRepr<T> {
-    _bytes: Vec<u8>,
-    _p: PhantomData<T>,
+    bytes: Vec<u8>,
+    p: PhantomData<T>,
 }
